501a501a7614fe5f07a0cd31b4ba3fa5cf0623fc
VilimKrajinovic <vilim.krajinovic@gmail.com>
2024-10-08 11:03:51 +0200

    Added folke/which-key as a plugin.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,6 +26,24 @@ return {
             require('lsp-progress').setup()
         end
     },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     { "scottmckendry/cyberdream.nvim", lazy = false, priority = 1000 },
     { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
