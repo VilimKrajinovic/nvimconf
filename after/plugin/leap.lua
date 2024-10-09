eeae3a0165db5ef04c9c14f93aadddc14e734f52
VilimKrajinovic <vilim.krajinovic@gmail.com>
2024-10-10 00:05:12 +0200

    Added leap.nvim config, <leader>s bidirectional

--- after/plugin/leap.lua
+++ after/plugin/leap.lua
@@ -1,1 +1,4 @@
-require('leap').create_default_mappings()
+vim.keymap.set('n',        '<leader>s', '<Plug>(leap)')
+vim.keymap.set('n',        '<leader>S', '<Plug>(leap-from-window)')
+vim.keymap.set({'x', 'o'}, '<leader>s', '<Plug>(leap-forward)')
+vim.keymap.set({'x', 'o'}, '<leader>S', '<Plug>(leap-backward)')
