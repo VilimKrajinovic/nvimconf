5ba6ff0e1876b55e49ca6e2e48ad4b36e0b34b9d
vilimkrajinovic <vilim.krajinovic@almamedia.com>
2025-04-09 14:13:05 +0200

    Changed dashboard

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -6,6 +6,24 @@ return {
     opts = {
       scroll = { enabled = false },
       animate = { enabled = false },
+      dashboard = {
+        formats = {
+          key = function(item)
+            return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
+          end,
+        },
+        sections = {
+          { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
+          { title = "MRU", padding = 1 },
+          { section = "recent_files", limit = 8, padding = 1 },
+          { title = "MRU ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
+          { section = "recent_files", cwd = true, limit = 8, padding = 1 },
+          { title = "Sessions", padding = 1 },
+          { section = "projects", padding = 1 },
+          { title = "Bookmarks", padding = 1 },
+          { section = "keys" },
+        },
+      },
     },
   },
   {
@@ -28,9 +46,9 @@ return {
     },
   },
   {
-    'saghen/blink.cmp',
+    "saghen/blink.cmp",
     opts = {
-      signature = {enabled = true},
-    }
-  }
+      signature = { enabled = true },
+    },
+  },
 }
