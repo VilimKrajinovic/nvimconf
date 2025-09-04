8359ae2961a0440aa7a6a970cd25cfb5f1c4f6f5
vilimkrajinovic <vilim.krajinovic@almamedia.com>
2025-09-04 12:56:32 +0200

    blinkcmp: fixed issue where buffer autocompletion was disabled

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -93,6 +93,9 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
+        providers = {
+          lsp = {fallbacks = {}}
+        },
         default = { "lsp", "path", "snippets", "buffer" },
       },
 
